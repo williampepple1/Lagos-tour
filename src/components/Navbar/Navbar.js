4826d93ee800ef6e3fd43448c5f1146eceb953f2
williampepple1 <36817380+williampepple1@users.noreply.github.com>
2021-02-01 23:34:14 +0100

    update

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -6,14 +6,8 @@ function Navbar() {
              <img src="https://media.istockphoto.com/vectors/lagos-stamp-lagos-grunge-round-isolated-sign-vector-id1227342926" alt="Lagos" className="lagos-logo"/>
            <ul className="navlinks">
               <li>
-                  <a href="/" className="navlink">Home</a>
+                  <a href="/" className="navlink">Wanna visit fun places in Lagos?<br/> look below</a>
               </li>
-              <li>
-                  <a href="/" className="navlink">About</a>
-              </li> 
-              <li>
-                  <a href="/" className="navlink active">Tours</a>
-              </li> 
            </ul>
         </div>
     )
--- src/components/tourData.js
+++ src/components/tourData.js
@@ -1,25 +1,140 @@
 export const tourData = [
-{
-    id: 1,
-    town: "surulere",
-    img: 'https://storage.googleapis.com/zama_live_bucket/neighbourhood/images/Surulere.jpg',
-    info: 'Surulere is a residential and commercial Local Government Area located on the mainland of Lagos in Lagos State, Nigeria, with an area of 23 km2 (8.9 sq mi).'
-},
-{
-    id: 2,
-    town: "victoria island",
-    img: 'https://images.squarespace-cdn.com/content/v1/5bf48451620b851a77ad58d9/1556378831340-5ENO1T731ZQQ8Y2ILH69/ke17ZwdGBToddI8pDm48kDaHZYp47CPhb396iE5lBehZw-zPPgdn4jUwVcJE1ZvWQUxwkmyExglNqGp0IvTJZUJFbgE-7XRK3dMEBRBhUpw5MBVHakMoqW9fh8iiLdrPbFQW8Rx3hK2KfYfxvuh4pi3v4H0UKgXp3kljrHCS_Ck/Civic%2Bcentre%2BVI_web.jpg',
-    info: 'Victoria Island in Lagos is the business and financial center of this Nigerian city and an upmarket area located between the Lekki peninsula and Lagos Island.'
-},
-{
-    id: 3,
-    town: "yaba",
-    img: 'https://guardian.ng/wp-content/uploads/2020/08/yaba.jpg',
-    info: 'Yaba is a suburb located on Lagos Mainland, Lagos in Lagos State. There are several federal government institutions in the area,'
-},{
-    id: 4,
-    town: "ikoyi",
-    img: 'https://cf.bstatic.com/images/hotel/max1024x768/455/45599284.jpg',
-    info: 'Ikoyi is the most affluent neighborhood of Lagos, located in Eti-Osa Local Government Area.'
-}
-]
+    {
+        id: 1,
+        town: "nike art gallery",
+        img: 'https://hotels.ng/guides/wp-content/uploads/2017/08/nike4.jpg',
+        info: 'This art gallery is also a visitor’s favourite in Lagos. It is a three-story building filled with artworks from all over the country, all of which, are available for purchase. At the time of this writing, entry into Nike Art Gallery is free, which adds to the appeal for visitors on a budget'
+    },
+    {
+        id: 2,
+        town: "lekki conservation centre",
+        img: 'https://hotels.ng/guides/wp-content/uploads/2017/03/lekki-1.jpg',
+        info: 'If you google ‘fun places to visit in Lagos’, Lekki Conservation Centre is likely to top the list. The truth is that there aren’t that many places like this in Lagos – and there should be. The main attraction of the Conservation Centre these days is the canopy walkway however, if you are anything like me, you’ll love the board walk even more.'
+    },
+    {
+        id: 3,
+        town: "la campagne tropicana",
+        img: 'https://i0.wp.com/www.travelwithapen.com/wp-content/uploads/2019/04/La-Campagne-Tropicana-Lagos.jpeg?resize=1536%2C1153',
+        info: 'La Campagne Tropicana is another lovely beach resort to visit in Lagos. It is located towards the outskirts of Lagos and it can take a while to get there, especially if you are travelling from Lagos Mainland. The appeal of La Campagne Tropicana is the clean and beautiful beach the property is on, as well as the availability of several activities to engage in. This is the only other location in Lagos that I think deserves to call itself a resort'
+    },{
+        id: 4,
+        town: "get arena",
+        img: 'https://hotels.ng/guides/wp-content/uploads/2017/03/open-uri20160921-22840-186zz2j.jpg',
+        info: 'It is located directly opposite the Palms shopping mall. Go-karting is a typical example of a break from the norm and children will also attest to this while they enjoy it. They also have a bar and an arcade with a wall climbing section. Worried about what fun spots in Lagos to visit? The GET Arena would be an awesome choice.'
+    },{
+        id: 5,
+        town: "freedom park",
+        img: 'https://hotels.ng/guides/wp-content/uploads/2017/03/freedom-park-lagos-04-450x300.jpg',
+        info: 'Freedom Park is a memorial and leisure park area in the middle of downtown Lagos. Built on a road that was formerly the broad street prison, it has now evolved into a leisure park where people organize shows and concert.'
+    },{
+        id: 6,
+        town: "rufus and bee",
+        img: 'https://i2.wp.com/www.travelwithapen.com/wp-content/uploads/2020/08/RufusandBee-VI-scaled.jpg?resize=1536%2C1152',
+        info: 'Rufus and Bee is a game arcade and bowling centre located in Victoria Island, Lagos. This is definitely a place worth visiting to let your hair down and activate your inner child.'
+    },{
+        id: 7,
+        town: "johnson jakande tinubu park",
+        img: 'https://akinwunmiambode.com/wp-content/uploads/2017/12/PIX-6695.jpg',
+        info: 'Because there aren’t many public parks in Lagos (and also because Lagos is overpopulated), this park can get very crowded. Nevertheless, it is a great spot for a picnic if you visit on a good day. Be aware that part of the park adjoins a major road. If blaring horns are unbearable for you, then you should visit, take your Instagram photos and escape to somewhere with more peace and quiet.'
+    },{
+        id: 8,
+        town: "lekki leisure lake",
+        img: 'https://hotels.ng/guides/wp-content/uploads/2017/03/lekki-leisure-lake.jpg-via-Travelstartnigeria.jpg',
+        info: 'Encourage your children to get active and engage their muscles by jumping on the quad bikes. They can also get on the Jet Ski with a guide. This place is equally enjoyable for adults. There are benches and chairs to sit on while enjoying food at the lakeside. Lekki Leisure Lake is a great place to visit on weekends and holidays.'
+    },{
+        id: 9,
+        town: "fun topia",
+        img: 'https://hotels.ng/guides/wp-content/uploads/2017/03/Funtopia-Water-Park_Lekki.jpg',
+        info: 'This is another amazing place designed to gain the interest of children. Even adults also get an opportunity to release the inner children in them. It’s a recreational park where families can come to do three things –have fun, have fun, and have more fun. There is a wide array of exciting activities to choose from such as swimming, games, bouncy castles, exhibitions and much more'
+    },{
+        id: 10,
+        town: "silverbird galleria",
+        img: 'https://hotels.ng/guides/wp-content/uploads/2017/03/Silverbird-Galleria-shut-by-AMCON-768x432.jpe',
+        info: 'Visit Silverbird Galleria to see a movie. There are also a host of shops there to shop at and nice restaurants to satisfy your food cravings'
+    },{
+        id: 11,
+        town: "federal palace hotel",
+        img: 'https://hotels.ng/guides/wp-content/uploads/2017/03/federal-palace-lobby.jpg',
+        info: 'Federal Palace Hotel is a 150-room hotel established in 1960. The hotel overlooks the Atlantic Ocean and is located in the commercial hub of Victoria Island in Lagos. The hotel is regarded as one of the top spots to visit, as it brings this air of class and luxury all the way!'
+    },{
+        id: 12,
+        town: "rele art gallery",
+        img: 'https://hotels.ng/guides/wp-content/uploads/2017/03/3e27ed7863c39223ca10f6939a47058c.jpg',
+        info: 'Rele Art Gallery is located at Onikan, Lagos. Opened in 2010, it is a gallery that delights itself in showcasing artworks of young contemporary artists with unconventional arts exhibitions'
+    },{
+        id: 13,
+        town: "seven eagles spur",
+        img: 'http://www.travelstart.com.ng/blog/wp-content/uploads/2014/06/Seven-Eagles-Spur.jpg',
+        info: 'Seven Eagles Spur serves variety of meals that you’ll love to have on your favourite plate at home. There’s apparently a Naija tint to every dish served, even though they serve Mexican foods. Jollof rice here is like it’s made in heaven, served with fresh salad – fresh vegetables and flavoured ingredients.'
+    },{
+        id: 14,
+        town: "dominos's pizza",
+        img: 'https://nairametrics.com/wp-content/uploads/2019/04/Dominos-Pizza-Cold-Stone-e1554450788413.jpg',
+        info: 'the restaurant offers a broad dine-in menu as well as take-away menu, you can easily order for pizza and have it delivered in your home. Their menu is one of the great dinning deals you can ever find around Lagos mainland, featuring a variety or Pizzas with flavourable sauce. '
+    },{
+        id: 15,
+        town: "pool terrace",
+        img: 'http://www.travelstart.com.ng/blog/wp-content/uploads/2014/06/PoolBarjpg861x588.jpg',
+        info: 'ool Terrace Bar offers a mix of fun, music and food. The restaurant is beautiful and tasty enough to make anyone hungry. It is known for excellent food and lively environment. You can enjoy great food –  Nigerian delicacies, barbecues and grilled food as well as live music. '
+    },{
+        id: 16,
+        town: "ocean basket",
+        img: 'https://hotels.ng/guides/wp-content/uploads/2017/04/Inside.jpg',
+        info: 'Located at 1392 Tiamiyu Savage Street, Victoria Island, Lagos, this Seafood restaurant has an extensive menu that contains whatever seafood delight you want. If your date loves seafood and a cozy ambiance, this is one of the perfect spots to go to. Also, if you have never tried seafood before and want to be adventurous on this date, this is one of the best places to experiment with.'
+    },{
+        id: 17,
+        town: "ember creek waterfront",
+        img: 'https://hotels.ng/guides/wp-content/uploads/2017/04/mbe.jpg-221-56f02687d0675.jpg',
+        info: 'Ember Creek Waterfront is a restaurant and bar that transforms into an exciting and lively nightclub at night. It is located at 32, Awolowo Road, Obalende, Ikoyi, Lagos. If your date likes to dance to booming Afro-pop music, this is one of the safest and most lively bars to visit and get your groove on.'
+    },{
+        id: 18,
+        town: "lihao chinese cuisine",
+        img: 'https://hotels.ng/guides/wp-content/uploads/2017/04/lihao-chinese-cuisine-restaurant-review-lagos-cassiedaves-1-768x508.jpg',
+        info: 'Lihao Chinese Cuisine is right in the heart of the mainland, situated at 2, Thorburn Avenue, Yaba, Lagos. Located on the ground floor of the Niger Palace Hotel and on the same street as the Afara Leadership Centre. Offers a  cozy oriental style restaurant with an extensive range of  Asian cuisine.'
+    },{
+        id: 19,
+        town: "cafe neo",
+        img: 'https://hotels.ng/guides/wp-content/uploads/2017/03/CAFE-NEO-cre8-2-768x511.jpg',
+        info: 'This is a cool coffee spot to go on a short date at. They have different outlets in Lagos but the favorites for young people are the one on the mainland at the E-Centre, Yaba, and Sanusi Fafunwa Street, Victoria Island. They have a variety of coffee and health drinks and pastry that are of great value for their price.'
+    },{
+        id: 20,
+        town: "cold stone creamery",
+        img: 'https://hotels.ng/guides/wp-content/uploads/2017/04/contact_ikeja3-4144-585a20d143f6b-768x511.jpg',
+        info: 'The Cold Stone Creamery is on the top floor of the same story-building as the Domino’s Pizza mentioned above. Pizza and ice-cream is one of the best combos known to man and if you are on a date with someone who has a sweet tooth, this is one of the most budget-friendly places to go.'
+    },{
+        id: 21,
+        town: "maryland mall",
+        img: 'https://hotels.ng/guides/wp-content/uploads/2017/04/maryland-mall-22-768x539.jpg',
+        info: 'This Maryland Mall hosts a lot of restaurants, bookstores, fashion and make-up stores, and health lounges. it is located at 350 – 360, Ikorodu Road, Anthony Village, Lagos.  It is the cool new spot for youths and was established in 2016. It is a good place to have a full-day date. You can see a movie in one of the cinema halls, shop at Shoprite or one of the other stores, walk around and window shop, and grab some food at one of the restaurants in the food court.'
+    },{
+        id: 22,
+        town: "sweet kiwi",
+        img: 'https://hotels.ng/guides/wp-content/uploads/2017/04/kiwi-31.jpg',
+        info: 'Sweet Kiwi is a Yogurt and Waffle spot located at Admiralty Way, Lekki Phase 1, Lagos. It is a very good spot to go on a brunch date especially if you or your date are Waffle, Yogurt, or generally, pastry lovers. Their yogurts are visually appealing and intensely tasty.'
+    },{
+        id: 23,
+        town: "tarkwa bay",
+        img: 'https://hotels.ng/guides/wp-content/uploads/2017/04/Screen-Shot-2016-10-16-at-7.17.17-PM.png',
+        info: 'Tarkwa Bay is a lovely, aesthetically pleasing beach. It is a very romantic spot that is located near the Lagos harbor. There are so many outdoor activities to do like Horseback riding, speedboat riding, and so on. If you are going out with someone that likes physical activity, this is a good spot to hang out. Just remember to wear light clothing and take a good camera for pictures'
+    },{
+        id: 24,
+        town: "melting moments",
+        img: 'https://hotels.ng/guides/wp-content/uploads/2017/03/service-melting-moments-04-1109-57870166bf3c4.jpg',
+        info: 'This creamery is for those who are sweet-toothed. They have outlets on the Island and the mainland. Their mainland location is at Shop U02, by Silverbird Cinema, Ikeja City Mall, Alausa, Ikeja. They have a wide variety of flavors, from mint choc chip to vanilla with sprinkles, chunky monkey, cookies and cream, and so on! But it definitely does not end there, they have other dessert items and you can also buy cakes for different celebrations here.'
+    },{
+        id: 25,
+        town: "chicken republic",
+        img: 'https://hotels.ng/guides/wp-content/uploads/2017/04/chicken_republic3.jpg',
+        info: 'Chicken Republic is one of the largest chicken restaurant chain in Nigeria. It has over 26 outlets in Lagos, Nigeria. If you are looking to treat your date to delicious chicken recipes, pastries, and ice-cream, Chicken Republic is a great choice. It also affords you privacy for an intimate discussion with your date.'
+    },{
+        id: 26,
+        town: "yellow chilli",
+        img: 'https://lh3.googleusercontent.com/proxy/AZt7GJzxXJrRYUVrpjrgEfx7WvZ-TE--oso5TT_khDfGjAFhNtyp9Fe_1I32nz_DdQCm2Ci5RMKTtRcJPlcR3Op4RCL0C6rqJMCjVMlvMK7QOf65eg',
+        info: 'Looking for a place where you can enjoy exquisite African and Pan-African delicacies? Yellow Chilli tops the list.  You get to enjoy African cuisines like Seafood Okro, Afang Soup, Banga Soup, Efo Riro, Edikan Ikong Soup, Otong soup, and many others. This is the location for a unique African-themed date. It is located at 27 Bishop Oluwole St. Off Bishop Oluwole St., Lagos.'
+    },{
+        id: 27,
+        town: "cactus restaurant",
+        img: 'https://hotels.ng/guides/wp-content/uploads/2017/04/Cactus-Restaurant-Confectionaries-Display-hotels.ng_-400x300.jpg',
+        info: 'Cactus Restaurant is well known for its tasty dishes and excellent services.  You can enjoy a wide variety of African cuisines, vegetarian food as well as pastries. It is a decent restaurant that allows you and your date privacy for a romantic date. You can find Cactus Restaurant at 20/24 Ozumba Mbadiwe Street, Victoria Island, Lagos 100103 Nigeria.'
+    }
+    ]
